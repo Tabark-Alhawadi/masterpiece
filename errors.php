e15f07779ffda99203b480b3cbba531104dc5f57
TabarkAlhawadi <tabarkalhawadi1@gmail.com>
2023-01-21 00:42:07 +0300

    explore page

--- errors.php
+++
@@ -1,7 +0,0 @@
-<?php if(count ($errors) > 0) : ?>
-    <div>
-        <?php foreach ($errors as $srror):?>
-            <p><?php echo $error?></p>
-        <?php endforeach ?>
-    </div>
-    <?php endif ?>
